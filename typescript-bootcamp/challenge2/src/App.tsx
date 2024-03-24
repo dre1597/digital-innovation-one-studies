71bfc658dbf614d63c08e3c0967d55ec496b18f3
André Pereira <andrepereira97@yahoo.com>
2024-03-24 02:07:15 -0300

    :sparkles: complete challenge 2

--- typescript-bootcamp/challenge2/src/App.tsx
+++ typescript-bootcamp/challenge2/src/App.tsx
@@ -1,23 +1,15 @@
-import './App.css';
-import { Box, Button, Center, Input } from '@chakra-ui/react';
-import { login } from './services/login.tsx';
+import { Box } from '@chakra-ui/react';
+import { LoginCard } from './components/LoginCard.tsx';
+import { Header } from './components/Header.tsx';
 
 function App() {
   return (
-    <Box minHeight="100vh" backgroundColor="#9413dc" padding="25px">
-      <Box backgroundColor="#FFFFFF" borderRadius="25px" padding="15px">
-        <Center>
-          <h1>Login</h1>
-        </Center>
-        <Input placeholder="email"/>
-        <Input placeholder="password"/>
-        <Center>
-          <Button onClick={login} colorScheme="teal" size="sm" width="100%" marginTop="5px">
-            Login
-          </Button>
-        </Center>
+    <>
+      <Header/>
+      <Box minHeight="100vh" backgroundColor="#9413dc" padding="25px">
+        <LoginCard/>
       </Box>
-    </Box>
+    </>
   );
 }
 
--- typescript-bootcamp/challenge2/src/components/Header.tsx
+++ typescript-bootcamp/challenge2/src/components/Header.tsx
@@ -1,7 +1,17 @@
+import { Box } from '@chakra-ui/react';
+
 export const Header = () => {
-  return(
-    <div>
+  return (
+    <Box
+      width="100%"
+      padding="10px"
+      color="white"
+      backgroundColor="#9413dc"
+      textAlign="center"
+      fontSize="20px"
+      fontWeight="bold"
+    >
       Dio Bank
-    </div>
-  )
-}
+    </Box>
+  );
+};
