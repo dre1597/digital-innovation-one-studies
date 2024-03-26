b988e96386bacd6eca087e919ebff9a159885138
André Pereira <andrepereira97@yahoo.com>
2024-03-26 08:06:59 -0300

    :sparkles: add variables

---
+++ kotlin-bootcamp/helloWorld/src/variables.kt
@@ -0,0 +1,21 @@
+fun someCondition(): Boolean {
+  return true
+}
+
+fun main() {
+  var a: String = "initial"
+  println(a)
+  val b: Int = 1
+  val c = 3
+
+  var e: Int
+
+  //  println(e) // generates an error
+  val d: Int = if (someCondition()) {
+    1
+  } else {
+    2
+  }
+
+  println(d)
+}
