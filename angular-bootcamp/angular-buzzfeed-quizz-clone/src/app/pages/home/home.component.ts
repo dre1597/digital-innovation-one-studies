e9e7a9383f3549e8a2d98a78a6605912a7eadee4
André Pereira <andrepereira97@yahoo.com>
2024-03-26 06:55:57 -0300

    :sparkles: add buzzfeed quizz clone project

---
+++ angular-bootcamp/angular-buzzfeed-quizz-clone/src/app/pages/home/home.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { QuizzComponent } from '../../components/quizz/quizz.component';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [
+    QuizzComponent
+  ],
+  templateUrl: './home.component.html',
+})
+export class HomeComponent {
+
+}
