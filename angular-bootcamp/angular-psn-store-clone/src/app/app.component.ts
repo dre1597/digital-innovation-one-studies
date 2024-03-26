cf745a935f183407fec618f1307265f5e1ddd7ad
André Pereira <andrepereira97@yahoo.com>
2024-03-26 06:46:31 -0300

    :sparkles: add psn store clone

---
+++ angular-bootcamp/angular-psn-store-clone/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
+import { HomeComponent } from './pages/home/home.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, MenuBarComponent, HomeComponent],
+  templateUrl: './app.component.html',
+})
+export class AppComponent {
+}
