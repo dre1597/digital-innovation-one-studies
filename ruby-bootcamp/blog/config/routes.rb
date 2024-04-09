61db20c8112c820e356fb880935a1e860c07d10c
André Pereira <andrepereira97@yahoo.com>
2024-04-09 13:26:48 -0300

    :sparkles: set root route, add home controller and view

--- ruby-bootcamp/blog/config/routes.rb
+++ ruby-bootcamp/blog/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'home/index'
+  root "home#index"
   resources :posts
   get "/articles", to: "articles#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
