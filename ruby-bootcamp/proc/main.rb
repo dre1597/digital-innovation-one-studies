f6b9dfb175767788e5325f171fffd9377dcffbfa
André Pereira <andrepereira97@yahoo.com>
2024-04-09 23:54:54 -0300

    :sparkles: add proc examples

---
+++ ruby-bootcamp/proc/main.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+hello_proc = proc do
+  puts 'Hello, World!'
+end
+
+hello_proc.call
+
+
+# hello_proc = Proc.new do
+#   puts 'Hello, World!'
+# end
+#
+# hello_proc.call
