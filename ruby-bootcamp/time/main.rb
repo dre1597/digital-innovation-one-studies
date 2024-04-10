1b951b71fe60a33afa355759075223fa99d765ab
André Pereira <andrepereira97@yahoo.com>
2024-04-10 00:14:45 -0300

    :sparkles: improve math and time examples

--- ruby-bootcamp/time/main.rb
+++ ruby-bootcamp/time/main.rb
@@ -1,14 +1,3 @@
-# frozen_string_literal: true
-
-puts Math.sqrt(16)
-
-puts Math.log10(100)
-
-puts radian = 30 * (Math::PI / 180)
-puts Math.sin(radian)
-
-puts Math::E
-
 time = Time.now
 
 puts time.year
