9cbc1c4cceed5952cb1cbfb9d3d0cd8ac1f6da13
André Pereira <andrepereira97@yahoo.com>
2024-04-19 23:59:34 -0300

    :sparkles: start adding exceptions

---
+++ spring-web-flux-bootcamp/reactive-flashcards/src/main/java/org/example/reactiveflashcards/domain/exception/NotFoundException.java
@@ -0,0 +1,8 @@
+package org.example.reactiveflashcards.domain.exception;
+
+public class NotFoundException extends ReactiveFlashcardsException {
+
+  public NotFoundException(final String message) {
+    super(message);
+  }
+}
---
+++ spring-web-flux-bootcamp/reactive-flashcards/src/main/java/org/example/reactiveflashcards/domain/exception/ReactiveFlashcardsException.java
@@ -0,0 +1,8 @@
+package org.example.reactiveflashcards.domain.exception;
+
+public class ReactiveFlashcardsException extends RuntimeException {
+
+  public ReactiveFlashcardsException(final String message) {
+    super(message);
+  }
+}
