04ae1a2b3fe0f96528bf269908a7ff431c365a1e
André Pereira <andrepereira97@yahoo.com>
2024-04-21 23:01:59 -0300

    :tada: start project

---
+++ spring-web-flux-bootcamp/reactive-bingo/src/main/java/org/example/reactivebingo/ReactiveBingoApplication.java
@@ -0,0 +1,12 @@
+package org.example.reactivebingo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveBingoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveBingoApplication.class, args);
+	}
+}
---
+++ spring-web-flux-bootcamp/reactive-bingo/src/test/java/org/example/reactivebingo/ReactiveBingoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.reactivebingo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveBingoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
