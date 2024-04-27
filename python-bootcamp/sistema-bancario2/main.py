24d98f457facd35765889efbe74f7a77d533fada
André Pereira <andrepereira97@yahoo.com>
2024-04-27 01:23:39 -0300

    :sparkles: add challenge 2

--- python-bootcamp/sistema-bancario2/main.py
+++ python-bootcamp/sistema-bancario2/main.py
@@ -1,12 +1,15 @@
 def exibir_menu():
     menu = """
-        [1] Depositar
-        [2] Sacar
-        [3] Extrato
-        [4] Sair
-        
-        =>
-    """
+[1] Depositar
+[2] Sacar
+[3] Extrato
+[4] Criar Usuário
+[5] Criar Conta
+[6] Listar Contas
+[0] Sair
+
+=>
+"""
     return input(menu)
 
 
@@ -54,12 +57,52 @@ def exibir_extrato(saldo, /, *, extrato):
     print("==========================")
 
 
+def criar_usuario(usuarios):
+    cpf = input("Digite seu CPF (somente números): ")
+    usuario = verificar_se_usuario_existe(usuarios, cpf)
+
+    if usuario:
+        print("Usuário já existente")
+        return
+
+    nome = input("Digite seu nome completo: ")
+    data_nascimento = input("Digite sua data de nascimento (dd-mm-aaaa): ")
+    endereco = input("Digite seu endereço (logradouro, numero - bairro - cidade/sigla estado): ")
+
+    usuarios.append({"nome": nome, "data_nascimento": data_nascimento, "cpf": cpf, "endereco": endereco})
+
+    print("Usuário criado com sucesso")
+
+
+def verificar_se_usuario_existe(usuarios, cpf):
+    usuarios_filtrados = [usuarios for usuario in usuarios if usuario["cpf"] == cpf]
+
+    return usuarios_filtrados[0] if usuarios_filtrados else None
+
+
+def criar_conta(agencia, numero_conta, usuarios):
+    cpf = input("Digite seu CPF (somente números): ")
+    usuario = verificar_se_usuario_existe(usuarios, cpf)
+
+    if usuario:
+        return {"agencia": agencia, "numero_conta": numero_conta, "usuario": usuario}
+
+    print("Usuário não encontrado, fluxo de criação de conta encerrado")
+
+
+def listar_contas(contas):
+    for conta in contas:
+        print(conta)
+
 def main():
     saldo = 0
     limite = 500
     extrato = ""
     numero_saques = 0
     LIMITE_SAQUES = 3
+    AGENCIA = "0001"
+    usuarios = []
+    contas = []
 
     while True:
         opcao = exibir_menu()
@@ -83,10 +126,24 @@ def main():
             exibir_extrato(saldo, extrato=extrato)
 
         elif opcao == "4":
+            criar_usuario(usuarios)
+
+        elif opcao == "5":
+            numero_conta = len(contas) + 1
+            conta = criar_conta(AGENCIA, numero_conta, usuarios)
+
+            if conta:
+                contas.append(conta)
+                print("Conta criada com sucesso!")
+
+        elif opcao == "6":
+            listar_contas(contas)
+
+        elif opcao == "0":
             break
 
         else:
-            print("Operação inválida! Por favor, selecione uma opção de 1 a 4.")
+            print("Operação inválida! Por favor, selecione uma opção de 0 a 4.")
 
 
 main()
