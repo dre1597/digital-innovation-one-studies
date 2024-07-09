8d2a54caeac50dda043d5279db634391a42e1dcf
André Pereira <andrepereira97@yahoo.com>
2024-07-09 20:40:51 -0300

    :sparkles: add mongodb config

---
+++ spring-web-flux-bootcamp/bingo-webflux/src/main/java/org/example/bingowebflux/BingoWebfluxApplication.java
@@ -0,0 +1,13 @@
+package org.example.bingowebflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BingoWebfluxApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(BingoWebfluxApplication.class, args);
+  }
+
+}
